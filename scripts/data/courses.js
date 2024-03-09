87a4c1290e7403ca744d86db865737221d7febd5
Waqar Kabir <waqarkabi@hotmail.com>
2024-03-09 19:08:18 +0500

    Datatable Implementation

---
+++ scripts/data/courses.js
@@ -0,0 +1,57 @@
+let courses = [
+    {
+        id: 1,
+        title: "C#",
+        duration: 1
+    },
+    {
+        id: 2,
+        title: "JS",
+        duration: 1.5
+    },
+    {
+        id: 3,
+        title: "HTML",
+        duration: 4
+    },
+    {
+        id: 4,
+        title: "CSS",
+        duration: 6
+    },
+    {
+        id: 5,
+        title: "PHP",
+        duration: 12
+    },
+    {
+        id: 6,
+        title: "SQL SERVER",
+        duration: 5
+    },
+    {
+        id: 7,
+        title: "REACT",
+        duration: 14
+    },
+    {
+        id: 8,
+        title: "ANGULAR",
+        duration: 18
+    },
+    {
+        id: 9,
+        title: "ROR",
+        duration: 4
+    },
+    {
+        id: 10,
+        title: "PEARL",
+        duration: 10
+    },
+    {
+        id: 11,
+        title: "R",
+        duration: 1
+    }
+];
